770626d3d6271fefd42a17d702eee98fd4ed1b0e
cn_liushuo <cn_liushuo@qq.com>
2025-01-18 11:35:49 +0800

    fix(HiView.vue): vue3中获取挂载到原型链上的数据

--- 14-vue3/02demoVite/src/views/HiView.vue
+++ 14-vue3/02demoVite/src/views/HiView.vue
@@ -1,5 +1,9 @@
 <script lang="ts" setup>
 import {useRoute} from "vue-router";
+import {getCurrentInstance} from "vue";
+// 相当于vue2的this
+const _this = getCurrentInstance()
+console.log(_this.appContext.config.globalProperties.$haha)
 
 console.log(useRoute().params.id)
 </script>
