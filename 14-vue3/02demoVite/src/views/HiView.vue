5ee51a468b7a8f307a20e6a39d1ea5fe3958d2a3
cn_liushuo <cn_liushuo@qq.com>
2025-01-18 11:28:41 +0800

    feat(HiView): 子路由内跳转页面

---
+++ 14-vue3/02demoVite/src/views/HiView.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup>
+import {useRoute} from "vue-router";
+
+console.log(useRoute().params.id)
+</script>
+
+<template>
+  <h1>123</h1>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
