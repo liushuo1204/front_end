a7bb441a9889f9f3da5f21cd0d63390cbdc4e1e3
cn_liushuo <cn_liushuo@qq.com>
2025-01-20 10:57:16 +0800

    feat: websocket在vue3中的使用方法

--- 14-vue3/01-demo/src/App.vue
+++ 14-vue3/01-demo/src/App.vue
@@ -1,31 +1,74 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-    |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div v-for="(item, index) in list" :key="index">
+    <p :class="item.userName != 'zs' ? 'nomy': 'my'">{{ item.userName }}：{{ item.data }}</p>
+  </div>
+  <div style="position: fixed;bottom: 0;">
+    <input v-model="msg" type="text">
+    <button @click="send">发送</button>
+  </div>
 </template>
+<script lang="ts" setup>
+import {ref} from 'vue';
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+let websocket: any;
+let list = ref([])
+let msg = ref('')
+
+// 初始化
+function init() {
+  websocket = new WebSocket("ws://124.220.152.89:8800/userName=zs")
+  //  监听sockt连接
+  // websocket.onopen = function(){
+  //   console.log("服务器链接成功~~")
+  // }
+  websocket.onopen = open
+  // 监听scoket信息
+  websocket.onmessage = message
+  // 监听scoket的错误信息
+  websocket.onerror = function () {
+    console.log("出错了~~~")
+  }
+  // 监听关闭连接
+  websocket.onclose = function () {
+    // 服务器断开连接
+    websocket.close()
+  }
 }
 
-nav {
-  padding: 30px;
+// 调用
+init()
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+function open() {
+  console.log("服务器链接成功~~")
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+function message(e: any) {
+  let obj = JSON.parse(e.data)
+  console.log(obj)
+  // 赋值聊天列表
+  list.value = obj.chatList
+}
+
+// 发送数据
+function send() {
+  websocket.send(JSON.stringify({
+    type: 2,
+    data: msg.value
+  }))
+}
+</script>
+
+<style lang="scss">
+.my {
+  float: right;
+  text-align: right;
+}
+
+.nomy {
+  float: left;
+}
+
+p {
+  width: 100%;
 }
 </style>
